391889af742caf34bd6e7dd8662e2876777d4e0b
Omar Cano <omc2403@gmail.com>
2026-02-18 21:45:12 -0800

    Updated Quartz Config URL

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "https://omc03.github.io/2D-Platformer-Quartz-Page/",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
